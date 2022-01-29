c237b13f705263b825f04c70869741eb4ae54d59
SergiuDeveloper <sergiu.nistor@info.uaic.ro>
2022-01-29 03:00:48 +0200

    Added technical report

---
+++ Docs/Scholarly/js/refs.js
@@ -0,0 +1,14 @@
+let refs = {
+    DBpedia: `
+    <dd property="schema:citation" typeof="schema:WebPage"
+        resource="https://www.dbpedia.org/">
+      <cite property="schema:name"><a href="https://www.dbpedia.org/">DBPedia - Global and Unified Access to Knowledge Graphs</a></cite>.
+    </dd>
+    `,
+    BiDaR: `
+    <dd property="schema:citation" typeof="schema:WebPage"
+        resource="https://profs.info.uaic.ro/~busaco/teach/courses/wade/projects/index.html#:~:text=concept%20%40%20Schema.org.-,bidar,-(big%20data%20retriever">
+      <cite property="schema:name"><a href="https://profs.info.uaic.ro/~busaco/teach/courses/wade/projects/index.html#:~:text=concept%20%40%20Schema.org.-,bidar,-(big%20data%20retriever">BiDaR project requirements</a></cite>.
+    </dd>
+    `
+};
